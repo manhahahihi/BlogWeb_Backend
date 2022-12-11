73d07ada26e022d6723c1839ec402d40d2ca18b3
Chanfuc <115201490+Chanfuc812@users.noreply.github.com>
2022-12-11 21:08:05 +0700

    update full Blog.js

--- models/Blog.js
+++ models/Blog.js
@@ -7,7 +7,14 @@ const blogSchema = new Schema({
     type: String,
     required: true,
   },
- 
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
   user: {
     type: mongoose.Types.ObjectId,
     ref: "User",
