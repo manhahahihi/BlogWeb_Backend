493d16934193227d22cc84e452698edf144191c4
manhahahihi <nmanh4138@gmail.com>
2022-12-16 11:03:11 +0700

    del somthin and create somthin

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
-import express from "express";
-import mongoose from "mongoose";
-import router from "./routes/userRoutes";
-import cors from "cors";
-import blogRouter from "./routes/blogRoutes";
+const express = require("express");
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use("/api/user", router);
-app.use("/api/blog", blogRouter )
-app.use(express.urlencoded({extended: true}));
-app.use(express.json())
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
-mongoose.set('strictQuery', true);
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
 mongoose
-    .connect("mongodb+srv://webapp:15102303@first.navtlma.mongodb.net/?retryWrites=true&w=majority"
-    )
-    .then(() => app.listen(5000))
-    .then(() =>
-        console.log("connected to database")
-    )
-    .catch((err) => console.log(err));
+  .connect("mongodb+srv://webapp:15102303@first.navtlma.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
