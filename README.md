3b2599f94c3c1b41ea91d7b4749d957a8bf6615c
Chanfuc <115201490+Chanfuc812@users.noreply.github.com>
2022-12-20 23:18:26 +0700

    update api full

--- README.md
+++
@@ -1,9 +0,0 @@
-## BACKEND-BLOG 💻
-
-## 🖥 Tech Stack:
-
-![nodejs](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)&nbsp;
-![expressjs](https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white)&nbsp;
-![mongodb](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)&nbsp;
-
-
--- models/User.js
+++ models/User.js
@@ -1,22 +1,27 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
-const userSchema = new Schema({
-    username:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    blogs:[{type: mongoose.Types.ObjectId, ref:"Blog", required: true}]
-});
-
-export default mongoose.model("Users", userSchema); 
+module.exports = mongoose.model("User", UserSchema);
